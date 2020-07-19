c972dea5e4044d022978f0ad921ef0acbe12add3
fshlarf <faishal.arif218@gmail.com>
2020-07-19 20:43:45 +0700

    chnage db logic

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,60 @@
+const dotenv = require("dotenv")
 const mysql = require('mysql')
 
-const seakunConn = mysql.createConnection({
-    password: 'S34kUn1D@2020',
-    user: 'u630538220_seakunid',
-    database: 'u630538220_seakun_main',
-    host: '185.224.137.8',
-    port: '3306',
-    multipleStatements: true
-})
+module.exports = class Established {
+    constructor () {
+        this.seakunConn = {}
+    }
 
-module.exports = seakunConn
+    async setEnv () {
+        dotenv.config()
+
+        return 1
+    }
+
+    async setConnection () {
+        this.seakunConn = mysql.createConnection({
+            host     : process.env.DB_HOST,
+            user     : process.env.DB_USER,
+            password : process.env.DB_PASS,
+            database : process.env.DB_NAME
+        })
+
+        return 1
+    }
+
+    async startConnection () {
+        this.seakunConn.connect(function(err) {
+            if (err) throw err
+        })
+
+        return 1
+    }
+
+    async endConnection () {
+        this.seakunConn.end()
+
+        return 1
+    }
+
+    async setQuery (callback) {
+        callback(this.seakunConn)
+
+        return 1
+    }
+
+    async play (callback) {
+        this.setEnv()
+        .then(() => {
+            this.setConnection()
+        })
+        .then(() => {
+            this.startConnection()
+        })
+        .then(() => {
+            this.setQuery(callback)
+        })
+
+        return 1
+    }
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,70 +1,75 @@
 const express = require('express')
-const seakunConn = require('../db')
+const Established = require('../db')
 
 const router = express.Router()
 
-seakunConn.connect((err) => {
-    if (!err)
-    console.log('DB connection succeded')
-    else
-    console.log('DB connection failed \n Error: ' + JSON.stringify(err, undefined, 2))
-})
-
 router.get('/', (req, res, next) => {
-    seakunConn.query('SELECT * FROM seakundb.registration', async (err, rows, fields) => {
-        if (!err) {
-            const responseRows = await rows
-            res.send(responseRows)
-        } else {
-            console.log(err)
-        }
+    new Established().play((seakunConn) => {
+        seakunConn.query('SELECT * FROM registration', async (err, rows, fields) => {
+            seakunConn.end()
+
+            if (!err) {
+                const responseRows = await rows
+                res.send(responseRows)
+            } else {
+                console.log(err)
+            }
+        })
     })
 })
 
 router.delete('/:id', (req, res) => {
-    seakunConn.query('DELETE FROM seakundb.registration WHERE id = ?', [req.params.id], async (err, rows, fields) => {
-        if (!err) {
-            res.send('Deleted Successfully')
-        } else {
-            console.log(err)
-        }
+    new Established().play((seakunConn) => {
+        seakunConn.query('DELETE FROM registration WHERE id = ?', [req.params.id], async (err, rows, fields) => {
+            seakunConn.end()
+
+            if (!err) {
+                res.send('Deleted Successfully')
+            } else {
+                console.log(err)
+            }
+        })
     })
 })
 
 router.post('/', (req, res, next) => {
-    let user = req.body
-    let sql = "SET @id = ?; SET @fullname = ?; SET @email = ?; SET @whatsapp = ?; SET @provider = ?; SET @packet = ?; SET @price = ?; \
-    CALL AddRegisteredUser(@id,@fullname,@email,@whatsapp,@provider,@packet,@price);" 
-    seakunConn.query(sql, [
-        user.id, 
-        user.fullname, 
-        user.email, 
-        user.whatsapp, 
-        user.provider, 
-        user.packet,
-        user.price
-    ], async (err, rows, fields) => {
-        if (!err) {
-            let result = {
-                code: 200,
-                message: '',
-                status: 'success',
-                data: null
-            }
-            const responseRows = await rows
-            responseRows.forEach(element => {
-                if (element.constructor == Array) {
-                    result.message = 'Inserted user id : ' +element[0].id
-                    res.send(result)
+    new Established().play((seakunConn) => {
+        let user = req.body
+        let sql = "SET @id = ?; SET @fullname = ?; SET @email = ?; SET @whatsapp = ?; SET @provider = ?; SET @packet = ?; SET @price = ?; \
+        CALL AddRegisteredUser(@id,@fullname,@email,@whatsapp,@provider,@packet,@price);" 
+        seakunConn.query(sql, [
+            user.id, 
+            user.fullname, 
+            user.email, 
+            user.whatsapp, 
+            user.provider, 
+            user.packet,
+            user.price
+        ], async (err, rows, fields) => {
+            seakunConn.end()
+
+            if (!err) {
+                let result = {
+                    code: 200,
+                    message: '',
+                    status: 'success',
+                    data: null
                 }
-            })
-        }
-        else {
-            console.log(err)
-            if (err.code == 'ER_DUP_ENTRY') {
-                res.send(err.msg = 'Error Duplicate Email')
+                const responseRows = await rows
+                responseRows.forEach(element => {
+                    if (element.constructor == Array) {
+                        result.message = 'Inserted user id : ' +element[0].id
+                        res.send(result)
+                    }
+                })
             }
-        }
+            else {
+                console.log(err)
+                if (err.code == 'ER_DUP_ENTRY') {
+                    res.send(err.msg = 'Error Duplicate Email')
+                }
+            }
+        })
     })
 })
 

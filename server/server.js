30024206785573a0d8841002a3567a1d7c94af7e
fshlarf <faishal.arif218@gmail.com>
2020-08-07 13:09:34 +0700

    update for fix blocked cross origin

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ const app = express()
 
 app.use(cors())
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+})
+
 app.use(express.json())
 
 app.use('/registered-user', apiRouter)

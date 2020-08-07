631cde07c730d56cee3c3aad6bde1827937bb2cc
fshlarf <faishal.arif218@gmail.com>
2020-08-07 13:41:25 +0700

    deploy heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors())
 
-pp.use((req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://seakun.id');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');

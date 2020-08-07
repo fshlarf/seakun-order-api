95b299e862d625c4fda895bf3c4e29256eb5703c
fshlarf <faishal.arif218@gmail.com>
2020-08-07 14:25:45 +0700

    update routes/index.js and server.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,14 @@ const Established = require('../db')
 
 const router = express.Router()
 
+const isNotCompleted = (body) => {
+    if (body.email === undefined || body.email === null || body.fullname === undefined || body.fullname === null) {
+        return true
+    }
+
+    return false
+}
+
 router.get('/', (req, res, next) => {
     new Established().play((seakunConn) => {
         seakunConn.query('SELECT * FROM registration', async (err, rows, fields) => {
@@ -34,42 +42,36 @@ router.delete('/:id', (req, res) => {
 
 router.post('/', (req, res, next) => {
     new Established().play((seakunConn) => {
-        let user = req.body
-        let sql = "SET @id = ?; SET @fullname = ?; SET @email = ?; SET @whatsapp = ?; SET @provider = ?; SET @packet = ?; SET @price = ?; \
-        CALL AddRegisteredUser(@id,@fullname,@email,@whatsapp,@provider,@packet,@price);" 
-        seakunConn.query(sql, [
-            user.id, 
-            user.fullname, 
-            user.email, 
-            user.whatsapp, 
-            user.provider, 
-            user.packet,
-            user.price
-        ], async (err, rows, fields) => {
-            seakunConn.end()
-
-            if (!err) {
-                let result = {
-                    code: 200,
-                    message: '',
-                    status: 'success',
-                    data: null
-                }
-                const responseRows = await rows
-                responseRows.forEach(element => {
-                    if (element.constructor == Array) {
-                        result.message = 'Inserted user id : ' +element[0].id
-                        res.send(result)
+        if (isNotCompleted(req.body)) {
+            res.send({
+                code: 400,
+                message: 'email or fullname not allowed empty',
+                status: 'EMPTY_REQ_BODY',
+                data: null
+            })
+        } else {
+            seakunConn.query('INSERT INTO registration SET ?', req.body, (err, rows, fields) => {
+                seakunConn.end()
+    
+                if (!err) {
+                    let result = {
+                        code: 200,
+                        message: 'success',
+                        status: rows,
+                        data: null
                     }
-                })
-            }
-            else {
-                console.log(err)
-                if (err.code == 'ER_DUP_ENTRY') {
-                    res.send(err.msg = 'Error Duplicate Email')
+                    res.send(result)
                 }
-            }
-        })
+                else {
+                    res.send({
+                        code: err.errno,
+                        message: err.sqlMessage,
+                        status: err.code,
+                        data: null
+                    })
+                }
+            })
+        }
     })
 })
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const apiRouter = require('./routes')
 const cors = require('cors')
+const bodyParser = require("body-parser")
 
 const app = express()
 
@@ -8,9 +9,12 @@ app.use(cors())
 
 app.use(express.json())
 
-app.options('/registered-user', cors())
+app.use(bodyParser.json({ type: "application/*+json" }))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use('/registered-user', apiRouter)
 
-app.listen(process.env.PORT || '4000', () => {
-    console.log(`Server is running on port: ${process.env.PORT || '4000'}`);
+app.listen(process.env.PORT || '7700', () => {
+    console.log(`Server is running on port: ${process.env.PORT || '7700'}`);
 })
